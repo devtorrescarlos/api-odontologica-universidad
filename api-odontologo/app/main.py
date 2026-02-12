74e14bc3d2f8c5c4c2e828d32b7ebf440006e731
Carlos Torres <carlos30847012@gmail.com>
2026-02-12 13:09:25 -0400

    feat: add cors configuration

--- api-odontologo/app/main.py
+++ api-odontologo/app/main.py
@@ -1,11 +1,26 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.modules.patients.router import router as patients_router
 from app.modules.dentists.router import router as dentists_router
 from app.modules.auth.router import router as auth_router
 from app.modules.appointments.router import router as appointments_router
 from app.modules.waiting_room.router import router as waiting_room_router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI();
+
+origins = os.getenv("CORS_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(patients_router, prefix="/patients", tags=["patients"])
